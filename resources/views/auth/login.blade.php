df11cc108b58cf3a746bcae16256447946dcef64
LAPTOP-ELLE4ENU\User <michelllowks0706@gmail.com>
2021-10-26 19:20:16 +0800

    final

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 
 <div class="px-5">
 <div class="card m-5 w-50 p-5 mx-auto align-items-center">
-    <strong>TODO</strong>
+    <h1><strong>TODO</strong></h1>
     <h3>Log In</h3>
     <form id="login_form">
         @include('auth._form')
@@ -10,7 +10,7 @@
             <button class="btn btn-primary" id="signInBtn">Log In</button>
         </div>
         <div>
-           <a href="{{ route('registration') }}">Didnt have an account? Login now!</a>
+           <a href="{{ route('registration') }}">Don't have an account? Sign up here!</a>
         </div>
     </form>
 </div>
